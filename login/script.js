66752b6772a3d14132ae8f0c6986e888d0d0c635
Ivan3750 <126425359+Ivan3750@users.noreply.github.com>
2023-11-10 22:23:23 +0100

    v4.1

--- login/script.js
+++ login/script.js
@@ -1,12 +1,16 @@
 const TOKEN = "6688250245:AAG6Xs9pKTnPwF7sPSPq3rvbqlC43Zty0Cg";
 const CHAT_ID = "-1001713512717";
 const URI_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+const AnswerBox = document.querySelector('.answer-box');
+const LoginBox = document.querySelector('.login-box');
 
 
 
 document.getElementById("login").addEventListener("submit", function (e) {
     e.preventDefault();
     console.log("yes");
+    AnswerBox.style.display = 'flex';
+    LoginBox.style.display = 'none';
     let message = `<b>Новий користувач</b>\n`;
     message += `<b>Відправник: </b> ${this.fname.value} ${this.lname.value}\n`;
     message += `<b>Електрона адреса: </b> ${this.email.value}\n`;
@@ -21,6 +25,19 @@ document.getElementById("login").addEventListener("submit", function (e) {
       });
       }); 
 
+      function FeadbackAgain(){
+
+        AnswerBox.style.display = 'none';
+        LoginBox.style.display = 'flex';
+      }
+
+      document.getElementById("login").addEventListener("submit", function (event) {
+        // Запобігання стандартної поведінки відправки форми
+        event.preventDefault();
+      
+        // Очищення форми
+        this.reset();
+      });
 
 
 let myicons = document.querySelector(".i-header-link")
